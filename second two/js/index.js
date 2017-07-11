60e4714d5bec3486c4443a2a2cd2bb601783383c
makerHo <524439109@qq.com>
2017-07-11 16:25:56 +0800

    contact left loop
    header loop 修改hover后动画累积执行问题

--- second two/js/index.js
+++ second two/js/index.js
@@ -1,4 +1,8 @@
 $(function(){
+	//声明db数据库
+	var db  = openDatabase("member","1.0","only",1024*1024*10,function(){});
+	
+	//header 轮播
 	var clitHeight=$(".header_gg_slides li").height();
 	var timers=setInterval(function(){
 		$(".header_gg_slides").animate({"top":"-"+clitHeight+"px"},500,function(){
@@ -19,56 +23,13 @@ $(function(){
 			})
 		},3000)	
 	});
-/**
- * sec_banner_gov 内容区轮播图 gov类型
- * */
-//loop test1
-//var eleWidth = $(".ban_gov li a img").width();
-//var secTimers;
-//  secTimers = setInterval(function(){
-//  	$(".ban_gov").animate({"left":"-"+eleWidth+"px"},500,function(){
-//  		$(this).css({"left":"0px"});
-//  		$(".ban_gov li:first").appendTo($(this));
-//  		$(".sec_ban_title li").css({})
-//  	})
-//  },3000);
-// 
-//  $(".sec_banner_gov").hover(function(){
-//  	console.log(secTimers)
-//  	clearInterval(secTimers);
-//  },function(){
-//  	secTimers = setInterval(function(){
-//  	$(".ban_gov").animate({"left":"-"+eleWidth+"px"},500,function(){
-//	    		$(this).css({"left":"0px"});
-//	    		$(".ban_gov li:first").appendTo($(this))
-//	    	})
-//	    },3000)
-//  });
-//
-
 
 // loop test2 bootstarp 控制间隔
 //设置切换时间间隔
 	$('.carousel').carousel({
 	  interval: 4000
 	})
-// loop test3
 
-//var secLoopTimer;
-//var eleWidth=$(".ban_gov li a img").width();
-//
-//
-//secLoopTimer= setInterval(function(){
-// 	$(".sec_ban_title li").each(function(index,value){
-//	   	if(index==$(".sec_ban_title li").length){
-//	   		index=0;
-//	   	}
-//	   	console.log(index)
-//	   	$(".sec_ban_title li").removeClass("sec_ban_title_change").eq(index).addClass("sec_ban_title_change");
-// 	});
-//},3000);
-
-//click change gov
 $('.sevGov').click(function(){
 	$(this).removeClass("slideswich").addClass("slideswich");
 	$(".secColl").removeClass("slideswich");
@@ -126,7 +87,9 @@ var newsContentWidth = $(".news_content div").width()+30;
 //console.log(newsContentWidth)
 $(".news_title li").hover(function(){
 	var lisIndex = $(this).index();
-	$(".news_content").animate({"left":"-"+newsContentWidth*lisIndex+"px"},300);
+	$(".news_content").animate({"left":"-"+newsContentWidth*lisIndex+"px"},200);
+},function(){
+	$(".news_content").stop(false,true);
 });
 /**
  *video tabs change 
